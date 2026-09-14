794ecc75ca2195581a0ec8073d15adc2600743fb
kiss <41434771@qq.com>
2026-09-14 15:41:22 +0800

    创建了一个最简单的agent

---
+++ src/01-chat.js
@@ -0,0 +1,19 @@
+import {input} from "@inquirer/prompts";
+import {ChatOpenAI} from "@langchain/openai";
+import { config } from 'dotenv'
+config()
+
+// console.log(process.env.OPENAI_API_KEY)
+// console.log(`Hello ${process.env.OPENAI_API_KEY}`)
+const model = new ChatOpenAI({
+    model: process.env.OPENAI_MODEL,
+    apiKey: process.env.OPENAI_API_KEY,
+    baseURL:process.env.OPENAI_BASE_URL
+});
+
+const question = await input({ message: 'Enter your question' });
+
+// const response = await model.invoke(new HumanMessage("Hello world!"));
+const response = await model.invoke(question)
+
+console.log(response)
